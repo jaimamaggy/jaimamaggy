411097cbaca1d429b9e54ebe0322a4d95af218c3
jaimamaggy <30maggy@gmail.com>
2019-07-11 19:58:36 -0400

    Premier cours de Java
    ouff

---
+++ js/premierscript.js
@@ -0,0 +1,17 @@
+//Créer par Maggy Gagné Lemire 11-07-2019
+
+//Déclaration des variables
+var nombre;
+var carre;
+var cube;
+
+//Lecture au clavier (l'usager entre une valeur)
+nombre = Number(prompt("Veuillez entrer un nombre svp : "));
+
+//Les calculs
+carre = nombre * nombre;
+cube = nombre * nombre * nombre;
+
+//Les affichages
+alert("Le carre du nombre est : " + carre);
+console.log("Le cube du nombre est " + cube);
