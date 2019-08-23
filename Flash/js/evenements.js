512722e39a88b927fd2520418af82206856fc92b
jaimamaggy <30maggy@gmail.com>
2019-08-22 20:27:31 -0400

    vérification des scripts

---
+++ Flash/js/evenements.js
@@ -0,0 +1,9 @@
+$('#flashID').css("position","relative");
+
+$( "#boutonGauche" ).click(function() {
+    $( "#boutonGauche" ).css("left", "50px");
+});
+
+$( "#boutonDroite" ).click(function() {
+    $( "#boutonDroite" ).css("left", "50px");
+});
