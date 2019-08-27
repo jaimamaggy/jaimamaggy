000904d55271e20ccd0cf52d0db76ff954af93d3
jaimamaggy <30maggy@gmail.com>
2019-08-26 20:42:15 -0400

    mise a jour 26 aout

--- Flash/js/evenements.js
+++ Flash/js/evenements.js
@@ -1,9 +1,41 @@
+//créer par maggy le 22-o8-2019
+
 $('#flashID').css("position","relative");
-
-$( "#boutonGauche" ).click(function() {
-    $( "#boutonGauche" ).css("left", "50px");
+var compteur=
+$('#boutonGauche').click(function() {
+    $( "#flashID" ).css("left",50);
 });
 
-$( "#boutonDroite" ).click(function() {
-    $( "#boutonDroite" ).css("left", "50px");
+$('#boutonDroite').click(function() {
+    $('#flashID').css("left",50);
 });
+
+$( "#" ).click(function() {
+    var nomImage = $('#flashID').attr("src");
+    alert(nomImage);
+    if(nomImage === "medias/bg.jpg")
+    {
+        $('#flashID').attr("src","medias/sw.jpg");
+    }
+    else
+        $('#flashID').attr("src","medias/bg.jpg");
+
+
+});
+
+$("#boutonEnleverImage").click(function() {
+    $('#flashID').hide();
+});
+
+$("#boutonAjouterImage").click(function() {
+    $('#flashID').show();
+});
+
+$(‘#boutonAjouterAttribut’).attr(“alt” , “photo de Flash”);
+
+
+$("#boutonAjouterTitre").click(function() {
+    $( 'body' ).prepend( "<h1>FLASH JQUERY</h1>" );
+});
+
+
