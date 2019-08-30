9d57ffe36d57640623084a674e5c6b8be5a0db79
jaimamaggy <30maggy@gmail.com>
2019-08-30 14:45:08 -0400

    Résultat final de Flash

--- Flash/js/evenements.js
+++ Flash/js/evenements.js
@@ -1,16 +1,18 @@
 //créer par maggy le 22-o8-2019
 
 $('#flashID').css("position","relative");
+$('#flashID2').hide();
+$('#nuke').hide();
+
 var compteur = 0;
-
-
 $('#boutonGauche').click(function() {
     compteur = compteur -50;
-    $( "#flashID" ).css("left",compteur);
+    $( "#flashID" ).css("left", compteur);
 });
 
 $('#boutonDroite').click(function() {
-    $('#flashID').css("left",-50);
+    compteur = compteur +50;
+    $('#flashID').css("left",compteur);
 });
 
 //*$( "#boutonChangerImage" ).click(function() {
@@ -54,8 +56,33 @@ $("#boutonRalentir").click(function() {
 
 $("#boutonTeleport").click(function() {
     $('#flashID').toggle();
-    $('#flashID2').css("opacity",".9");
+    $('#flashID2').toggle();
 
 });
+
+
+$("#boutonVitesseSupreme").click(function() {
+    var myvar = setInterval(disparaitreFlash, 500)
+
+});
+    function disparaitreFlash() {
+        $('#flashID').toggle();
+        setTimeout("disparaitreFlash()", 5000);
+ }
+
+$("#boutonNuke").click(function() {
+    $('#sectionbouton').hide();
+    $('#nuke').show();
+
+});
+
+
+
+
+
+
+
+
+
 
 
