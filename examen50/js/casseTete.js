5e5346d7550f8e9fe2a54d6478112ae758c54ad2
jaimamaggy <30maggy@gmail.com>
2019-09-05 20:13:06 -0400

    alerte ne fonctionne pas

--- examen50/js/casseTete.js
+++ examen50/js/casseTete.js
@@ -1,4 +1,29 @@
 /** Créer par Maggy Gagné Lemire / 29-08-2019 */
+
+//jeux-du-casse-tête
+var premierClique = 1;
+var premierImageSRC;
+var premiereImageID;
+var deuxiemeImageSRC;
+var deuxiemeImageID;
+
 $( ".img-fluid" ).click(function() {
-    alert( "Handler for .click() called." );
-});
+    alert("click sur une image")
+    //var idImage = this.id;
+    //var srcImage = this.src;
+    //alert(srcImage);
+
+    //if (premierClique === 0) {
+       // premierClique = 1;
+    }
+    //else
+        //premierClique = 0;
+
+//});
+
+//$("img").click(function() {
+    //$('').hide();
+//});
+
+//$("img").click(function() {
+ //   var id = this.id;;
